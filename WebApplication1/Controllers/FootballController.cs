d214ea0819a63f3b6c35185e8d9dd1e75eb7c189
maroldo26 <maroldo26@gmail.com>
2016-05-04 23:01:01 +0530

    initial source added

---
+++ WebApplication1/Controllers/FootballController.cs
@@ -0,0 +1,71 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AllScoresUI.Services;
+using Newtonsoft.Json.Linq;
+
+
+namespace AllScoresUI.Controllers
+{
+    public class FootballController : Controller
+    {
+        // GET: Football
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        //[ChildActionOnly]
+        public PartialViewResult Score()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        //[ChildActionOnly]
+        public ActionResult Table(int id)
+        {
+            try 
+	        {
+                object jsonvalue = AllScoreService.Instance.GetTable(id);
+                return PartialView(jsonvalue);
+            
+		
+	        }
+            catch (System.Web.Http.HttpResponseException ex)
+	        {
+                Response.StatusCode = (int)ex.Response.StatusCode;
+                return Json(ex.Response.ReasonPhrase, JsonRequestBehavior.AllowGet);		        
+	        }            
+        }
+
+        [HttpGet]
+        public object Leagues()
+        {
+            string leagues = AllScoreService.Instance.GetLeagues();
+            return leagues;
+        }
+
+        [HttpGet]
+        //[ChildActionOnly]
+        public ActionResult Fixtures(int id)
+        {
+            try
+            {
+                object jsonvalue = AllScoreService.Instance.GetNextSevenDayFixtures(id);
+                return PartialView(jsonvalue);
+
+
+            }
+            catch (System.Web.Http.HttpResponseException ex)
+            {
+                Response.StatusCode = (int)ex.Response.StatusCode;
+                return Json(ex.Response.ReasonPhrase, JsonRequestBehavior.AllowGet);
+            }
+        }
+    }
+}
---
+++ WebApplication1/Services/AllScoreService.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+
+namespace AllScoresUI.Services
+{
+    public class AllScoreService
+    {
+        private AllScoreService()
+        {
+
+        }
+
+        private static AllScoreService instance;
+
+        public static AllScoreService Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new AllScoreService();
+                }
+                return instance;
+            }
+        }
+
+        public string GetTable(int leagueid=1204)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                try
+                {
+                    return webClient.DownloadString(string.Format("http://localhost:60467/api/score/gettable/{0}", leagueid));
+                }
+                catch (WebException e)
+                {
+
+                    var stream = e.Response.GetResponseStream();
+                    using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                    {
+                        var message = reader.ReadToEnd();
+                        var resp = new HttpResponseMessage(HttpStatusCode.NotFound)
+                        {
+                            Content = new StringContent(message),
+                            ReasonPhrase = message
+                        };
+                        throw new HttpResponseException(resp);
+                    }                    
+                }                
+            }
+        }
+
+        public string GetNextSevenDayFixtures(int id)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                try
+                {
+                    //http://localhost:63823/api/score/getfixtures?competitionid=1204&fromdate=13.12.2015&enddate=15.12.2015
+                    DateTime fromdate, enddate;
+                    fromdate = DateTime.Today;
+                    enddate = fromdate.AddDays(7);
+                    return webClient.DownloadString(string.Format("http://localhost:60467/api/score/getfixtures?competitionid={0}&fromdate={1}&enddate={2}", id, 
+                        fromdate.ToString("d.M.yyyy"), 
+                        enddate.ToString("d.M.yyyy")));
+                }
+                catch (WebException e)
+                {
+
+                    var stream = e.Response.GetResponseStream();
+                    using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+                    {
+                        var message = reader.ReadToEnd();
+                        var resp = new HttpResponseMessage(HttpStatusCode.NotFound)
+                        {
+                            Content = new StringContent(message),
+                            ReasonPhrase = message
+                        };
+                        throw new HttpResponseException(resp);
+                    }
+                }
+            }
+        }
+
+        internal string GetLeagues()
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                return webClient.DownloadString(string.Format("http://localhost:60467/api/score/gettounaments"));
+            }
+        }
+    }
+}
